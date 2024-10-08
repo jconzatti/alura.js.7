29755a16add8fdb4407427ce0f4c1c20b1c28448
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-10-08 13:16:01 -0300

    interface do usuário

--- js/api.js
+++ js/api.js
@@ -1,7 +1,32 @@
-async function obterPensamentos(){
-    const lRespostaDePensamentos = await fetch("http://localhost:3000/pensamentos");
-    const lPensamentos = await lRespostaDePensamentos.json();
-    console.log(lPensamentos);
+const api = {
+    async obterPensamentos(){
+        try {
+            const lRespostaDePensamentos = await fetch("http://localhost:3000/pensamentos");
+            return await lRespostaDePensamentos.json();
+        } catch (lErro) {
+            alert(`Erro ao buscar seus pensamentos! ${lErro.name}: ${lErro.message}`); 
+            throw lErro;   
+        }
+    },
+    
+    async salvarPensamento(pPensamento){
+        try {
+            const lRespostaDePensamentos = await fetch(
+                "http://localhost:3000/pensamentos",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(pPensamento)
+                }
+            );
+            return await lRespostaDePensamentos.json();
+        } catch (lErro) {
+            alert(`Erro ao salvar seus pensamento! ${lErro.name}: ${lErro.message}`); 
+            throw lErro;   
+        }
+    }
 }
 
-obterPensamentos();
+export default api;
