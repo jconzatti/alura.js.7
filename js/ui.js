3e15fbf638d35c0d43d9ffcd16309a7311143670
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-10-10 12:21:15 -0300

    refac: privando um método
    que cria um elemento de pensamento no html

--- js/ui.js
+++ js/ui.js
@@ -4,38 +4,38 @@ const ui = {
     async renderizarPensamentos(){
         try {
             const lPensamentos = await api.obterPensamentos();    
-            lPensamentos.forEach(ui.adicionarPensamentoNaLista);
+            lPensamentos.forEach(adicionarPensamentoNaLista);
         } catch (lErro) {
             alert(`Erro ao renderizar seus pensamentos! ${lErro.name}: ${lErro.message}`); 
             throw lErro;   
         }
-    },
-
-    adicionarPensamentoNaLista(pPensamento){
-        const lElementoListaDePensamentos = document.getElementById("lista-pensamentos");
-
-        const lElementoItemDaListaDePensamentos = document.createElement("li");
-        lElementoItemDaListaDePensamentos.setAttribute("data-id", pPensamento.id);
-        lElementoItemDaListaDePensamentos.classList.add("li-pensamento");
-
-        const lElementoIconeAspas = document.createElement("img");
-        lElementoIconeAspas.src = "assets/imagens/aspas-azuis.png";
-        lElementoIconeAspas.alt = "Aspas azuis";
-        lElementoIconeAspas.classList.add("icone-aspas");
-
-        const lElementoConteudoDoPensamento = document.createElement("div");
-        lElementoConteudoDoPensamento.classList.add("pensamento-conteudo");
-        lElementoConteudoDoPensamento.textContent = pPensamento.conteudo;
-
-        const lElementoAutoriaDoPensamento = document.createElement("div");
-        lElementoAutoriaDoPensamento.classList.add("pensamento-autoria");
-        lElementoAutoriaDoPensamento.textContent = pPensamento.autoria;
-
-        lElementoItemDaListaDePensamentos.appendChild(lElementoIconeAspas);
-        lElementoItemDaListaDePensamentos.appendChild(lElementoConteudoDoPensamento);
-        lElementoItemDaListaDePensamentos.appendChild(lElementoAutoriaDoPensamento);
-        lElementoListaDePensamentos.appendChild(lElementoItemDaListaDePensamentos);
     }
+}
+
+function adicionarPensamentoNaLista(pPensamento){
+    const lElementoListaDePensamentos = document.getElementById("lista-pensamentos");
+
+    const lElementoItemDaListaDePensamentos = document.createElement("li");
+    lElementoItemDaListaDePensamentos.setAttribute("data-id", pPensamento.id);
+    lElementoItemDaListaDePensamentos.classList.add("li-pensamento");
+
+    const lElementoIconeAspas = document.createElement("img");
+    lElementoIconeAspas.src = "assets/imagens/aspas-azuis.png";
+    lElementoIconeAspas.alt = "Aspas azuis";
+    lElementoIconeAspas.classList.add("icone-aspas");
+
+    const lElementoConteudoDoPensamento = document.createElement("div");
+    lElementoConteudoDoPensamento.classList.add("pensamento-conteudo");
+    lElementoConteudoDoPensamento.textContent = pPensamento.conteudo;
+
+    const lElementoAutoriaDoPensamento = document.createElement("div");
+    lElementoAutoriaDoPensamento.classList.add("pensamento-autoria");
+    lElementoAutoriaDoPensamento.textContent = pPensamento.autoria;
+
+    lElementoItemDaListaDePensamentos.appendChild(lElementoIconeAspas);
+    lElementoItemDaListaDePensamentos.appendChild(lElementoConteudoDoPensamento);
+    lElementoItemDaListaDePensamentos.appendChild(lElementoAutoriaDoPensamento);
+    lElementoListaDePensamentos.appendChild(lElementoItemDaListaDePensamentos);
 }
 
 export default ui;
