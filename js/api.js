19e9e09130fb85aa5eb9cc01c90bf3db663615ab
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-10-10 13:37:59 -0300

    feat: api atualizar pensamento

--- js/api.js
+++ js/api.js
@@ -8,6 +8,16 @@ const api = {
             throw lErro;   
         }
     },
+
+    async obterPensamento(pIDDoPensamento){
+        try {
+            const lRespostaDePensamentos = await fetch(`http://localhost:3000/pensamentos/${pIDDoPensamento}`);
+            return await lRespostaDePensamentos.json();
+        } catch (lErro) {
+            alert(`Erro ao buscar o pensamento (id ${pIDDoPensamento})! ${lErro.name}: ${lErro.message}`); 
+            throw lErro;   
+        }
+    },
     
     async salvarPensamento(pPensamento){
         try {
@@ -23,9 +33,29 @@ const api = {
             );
             return await lRespostaDePensamentos.json();
         } catch (lErro) {
-            alert(`Erro ao salvar seus pensamento! ${lErro.name}: ${lErro.message}`); 
+            alert(`Erro ao salvar pensamento! ${lErro.name}: ${lErro.message}`); 
             throw lErro;   
         }
+    },
+
+    async atualizarPensamento(pPensamento){
+        try {
+            const lRespostaDePensamentos = await fetch(
+                `http://localhost:3000/pensamentos/${pPensamento.id}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(pPensamento)
+                }
+            );
+            return await lRespostaDePensamentos.json();
+        } catch (lErro) {
+            alert(`Erro ao atualizar pensamento! ${lErro.name}: ${lErro.message}`); 
+            throw lErro;   
+        }
+
     }
 }
 
